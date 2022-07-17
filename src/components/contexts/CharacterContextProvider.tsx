d15ce9c7b4ea6e3bad3a4266129a413a8532062e
DeathRight <only.deathright@gmail.com>
2022-07-16 20:41:50 -0500

    started adding character context; updated TS

---
+++ src/components/contexts/CharacterContextProvider.tsx
@@ -0,0 +1,7 @@
+import { CharacterContextProviderProps } from "../../util/component-props";
+
+export const CharacterContextProvider = (
+    props: CharacterContextProviderProps
+) => {
+    const;
+};
