df8e6198a6801c0d6c3a8cf9b19af7a17f65671a
DeathRight <only.deathright@gmail.com>
2022-07-16 22:56:05 -0500

    finished charactercontextprovider

--- src/components/contexts/CharacterContextProvider.tsx
+++ src/components/contexts/CharacterContextProvider.tsx
@@ -1,7 +1,123 @@
-import { CharacterContextProviderProps } from "../../util/component-props";
+import { useMemo, useReducer, useState } from "react";
+import Roll from "roll";
+import {
+    CharacterProvider,
+    NameReducer,
+    State,
+} from "../../contexts/character-context";
+import useUpdateEffect from "../../hooks/useUpdateEffect";
+import { genName, properNoun, Sex } from "../../util";
+import backgrounds from "../../util/backgrounds";
+import {
+    Character,
+    CharacterContextProviderProps,
+    StatNames,
+} from "../../util/component-props";
+
+const roll = new Roll();
+
+// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const randomInt = (a = 1, b = 0) => {
+    const lower = Math.ceil(Math.min(a, b));
+    const upper = Math.floor(Math.max(a, b));
+    return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
 
 export const CharacterContextProvider = (
     props: CharacterContextProviderProps
 ) => {
-    const;
+    const {
+        minAge = 1,
+        maxAge = 60,
+        statRoll = "4d6b3",
+        value,
+        children,
+    } = props;
+
+    /* --------------------------------- Biology -------------------------------- */
+    const [sex, setSex] = useState(
+        value?.sex ?? (Math.round(Math.random()) as Sex)
+    );
+    const [age, setAge] = useState(value?.age ?? randomInt(minAge, maxAge));
+
+    /* ------------------------------- Background ------------------------------- */
+    const [background, setBackground] = useState(
+        value?.background ?? backgrounds[randomInt(0, backgrounds.length - 1)]
+    );
+
+    /* ---------------------------------- Stats --------------------------------- */
+    const rollStats = () => {
+        const a = [];
+        for (let i = 0; i < StatNames.length; i++) {
+            a.push(roll.roll(statRoll));
+        }
+
+        return a;
+    };
+    const [stats, setStats] = useState(() => value?.stats ?? rollStats());
+
+    /* ---------------------------------- Names --------------------------------- */
+    const [gName] = useState(() => genName(sex));
+    const [name, dispatchName] = useReducer(NameReducer.reducer, {
+        firstName: value?.firstName ?? properNoun(gName.first()),
+        lastName: value?.lastName ?? properNoun(gName.last()),
+        gen: gName,
+    });
+
+    /* -------------------------------- Character ------------------------------- */
+    const char = useMemo(
+        () =>
+            ({
+                sex: sex,
+                age: age,
+                firstName: name.firstName,
+                lastName: name.lastName,
+                background: background,
+                stats: stats,
+                ...(() => {
+                    let o: { [k: string]: number } = {};
+                    stats.forEach((v, i) => (o[StatNames[i]] = v.result));
+                    return o;
+                })(),
+            } as Character),
+        [sex, age, name, stats, background]
+    );
+    //const json = useMemo(() => JSON.stringify(char, null, " "), [char]);
+
+    /* --------------------------------- Render --------------------------------- */
+    const state: State = useMemo(
+        () => ({
+            sex,
+            setSex,
+            age,
+            setAge,
+            background,
+            setBackground,
+            stats,
+            setStats,
+            name,
+            dispatchName,
+            char,
+        }),
+        [age, background, char, name, sex, stats]
+    );
+
+    useUpdateEffect(() => {
+        if (value && value !== char) {
+            setSex(value.sex);
+            setAge(value.age);
+            setBackground(value.background);
+            setStats(value.stats);
+            dispatchName({
+                type: "set",
+                payload: {
+                    firstName: value.firstName,
+                    lastName: value.lastName,
+                    gen: genName(value.sex),
+                },
+            });
+        }
+    }, [value]);
+
+    return <CharacterProvider value={state}>{children}</CharacterProvider>;
 };
--- src/contexts/character-context.tsx
+++ src/contexts/character-context.tsx
@@ -1,25 +1,26 @@
-import { createContext, useState } from 'react';
-import Roll from 'roll';
+import { createContext, Dispatch, SetStateAction } from "react";
+import Roll from "roll";
 
-import { genName, properNoun, Sex } from '../util';
-import backgrounds from '../util/backgrounds';
-import { Character } from '../util/component-props';
+import { genName, properNoun, Sex } from "../util";
+import backgrounds from "../util/backgrounds";
+import { Character } from "../util/component-props";
 
 export namespace NameReducer {
-    type Actions = "first" | "last" | "full";
-    export type Action = { type: Actions; payload?: Sex };
-    export type Dispatch = (action: Action) => void;
+    type Actions = "first" | "last" | "full" | "set";
     export type State = {
         firstName: string;
         lastName: string;
         gen: ReturnType<typeof genName>;
     };
+    export type Action = { type: Actions; payload?: Sex | State };
+    export type Dispatch = (action: Action) => void;
 
     export const reducer = (state: State, action: Action): State => {
         let { gen } = state;
         switch (action.type) {
             case "full": {
-                if (action.payload) gen = genName(action.payload);
+                if (action.payload && typeof action.payload === "number")
+                    gen = genName(action.payload);
                 return {
                     firstName: properNoun(gen.first()),
                     lastName: properNoun(gen.last()),
@@ -32,30 +33,56 @@ export namespace NameReducer {
             case "last": {
                 return { ...state, lastName: properNoun(gen.last()) };
             }
+            case "set": {
+                if (action.payload && typeof action.payload === "object")
+                    return action.payload;
+                else {
+                    console.warn(
+                        "Attempted to dispatch 'set' on name reducer without payload being an object!"
+                    );
+                    return state;
+                }
+            }
         }
     };
 }
 
 /**
  * Typing equivalent for `const [S, SS] = T` as an object
- * 
+ *
  * **Example**:
- * 
+ *
  * `Constize<[0, 1], "first", "second">`
- * 
+ *
  * results in:
- * 
+ *
  * `{first: number, second: number}`
  */
-type Constize<T extends [any, any], S extends string, SS extends string> = { [N in S]: NonNullable<T[0]> } & { [M in SS]: NonNullable<T[1]> };
-type Stateful<T> = ReturnType<typeof useState<T>>;
+type Constize<T extends [any, any], S extends string, SS extends string> = {
+    [N in S]: NonNullable<T[0]>;
+} & { [M in SS]: NonNullable<T[1]> };
+type Stateful<T> = [T, Dispatch<SetStateAction<T>>];
 /* ------------------------------------ * ----------------------------------- */
 type SexState = Constize<Stateful<Sex>, "sex", "setSex">;
 type AgeState = Constize<Stateful<number>, "age", "setAge">;
-type BgState = Constize<Stateful<typeof backgrounds[number]>, "background", "setBackground">
-type StatsState = Constize<Stateful<ReturnType<Roll["roll"]>[]>, "stats", "setStats">
+type BgState = Constize<
+    Stateful<typeof backgrounds[number]>,
+    "background",
+    "setBackground"
+>;
+type StatsState = Constize<
+    Stateful<ReturnType<Roll["roll"]>[]>,
+    "stats",
+    "setStats"
+>;
 
-type State = SexState & AgeState & BgState & StatsState & {name: NameReducer.State, dispatch: NameReducer.Dispatch} & {char: Character}
+export type State = SexState &
+    AgeState &
+    BgState &
+    StatsState & {
+        name: NameReducer.State;
+        dispatchName: NameReducer.Dispatch;
+    } & { char: Character };
 
 export const CharacterContext = createContext<State | undefined>(undefined);
 export const CharacterProvider = CharacterContext.Provider;
